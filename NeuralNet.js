63bf652ec5e275f57ba8fccd0930ff621e6b62fb
Nicholas Kinney <35116791+Nicknakin@users.noreply.github.com>
2018-06-18 22:13:56 -0500

    Neural Net Changes

--- NeuralNet.js
+++ NeuralNet.js
@@ -3,21 +3,29 @@ class NeuralNet{
     this.model = tf.sequential();
     this.model.add(tf.layers.dense({units: hiddenNodes, inputShape: [inputNodes]}));
     for(let i = 1; i < hiddenLayers; i++){
-       this.mode.add(tf.layers.dense({units: hiddenNodes}));
+       this.model.add(tf.layers.dense({units: hiddenNodes}));
     }
     this.model.add(tf.layers.dense({units: outputNodes}));
+    this.hiddenLayers = hiddenLayers;
+    this.model.compile();
   }
 
   predict(inputs){
-
+    return this.model.predict(inputs);
   }
 
   duplicate(){
-
+    let newModel = tf.sequential();
+    for(let i = 0; i <= this.hiddenLayers; i++){
+      newModel.add(this.model.getLayer(i));
+    }
+    return newModel;
   }
 
   mutate(){
+    for(let i = 0; i < this.hiddenLayers; i++){
 
+    }
   }
 
   dispose(){
